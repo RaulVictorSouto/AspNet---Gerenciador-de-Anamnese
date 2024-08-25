d10da6c051c5a8a470e5a270144e698e2756708c
Raul Souto <raulvprsouto@gmail.com>
2024-08-25 16:49:53 -0300

    Adicionar arquivos de projeto.

---
+++ Anamnese/Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Anamnese.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anamnese.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<PacienteModel> PacienteModel { get; set;}
+    }
+}
---
+++ Anamnese/Models/PacienteModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace Anamnese.Models
+{
+    [Table("Pacientes")]
+    public class PacienteModel
+    {
+        [Display(Name = "Código Paciente")]
+        [Column("IdPaciente")]
+        public int IdPaciente { get; set; }
+
+        [Display(Name = "Nome Paciente")]
+        [Column("NomePaciente")]
+        public string NomePaciente { get; set; }
+
+        public string SobrenomePaciente { get; set; }
+
+        public DateTime DataNascimentoPaciente { get; set; }
+
+        public string GeneroPaciente { get; set; }
+
+        public string CpfPaciente { get; set; }
+
+        public string RgPaciente { get; set; }
+
+        public string CertidaoPaciente { get; set; }
+
+        public string TelefonePaciente { get; set; }
+
+        public string CelularPaciente { get; set; }
+
+        public string EstadoCivilPaciente { get; set; }
+
+        public string CepPaciente { get; set; }
+
+        public string LogradouroPaciente { get; set; }
+
+        public string NumeroEnderecoPaciente { get; set; }
+
+        public string BairroPaciente { get; set; }
+
+        public string CidadePaciente { get; set; }
+
+        public string EstadoPaciente { get; set; }
+
+        public DateTime DataCadastroPaciente { get; set; }
+    }
+}
